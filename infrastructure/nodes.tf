e5a71209de214ce007b650c1cffc8028283ac661
Fabio Berchtold <jamesclonk@jamesclonk.ch>
2022-08-14 16:09:51 +0200

    Update nodes.tf

--- infrastructure/nodes.tf
+++ infrastructure/nodes.tf
@@ -29,6 +29,10 @@ resource "vcd_vapp_vm" "k8s_bastion" {
     allow_local_admin_password = true
     auto_generate_password     = false
     admin_password             = var.k8s_bastion_root_password
+    # TODO: or maybe this needs to be done like this: https://github.com/vmware/terraform-provider-vcd/issues/510#issuecomment-843721455
+    initscript                 = <<-EOT
+    ssh_pwauth: true
+    EOT
   }
 
   depends_on = [
@@ -52,6 +56,13 @@ resource "vcd_vapp_vm" "k8s_control_plane" {
   accept_all_eulas = true
   power_on         = true
 
+  override_template_disk {
+    bus_type        = "paravirtual"
+    size_in_mb      = "40960"
+    bus_number      = 0
+    unit_number     = 0
+  }
+  
   network {
     type               = "org"
     name               = vcd_network_routed_v2.k8s_nodes.name
@@ -64,6 +75,15 @@ resource "vcd_vapp_vm" "k8s_control_plane" {
     allow_local_admin_password = true
     auto_generate_password     = false
     admin_password             = var.k8s_control_plane_root_password
+    # TODO: or maybe this needs to be done like this: https://github.com/vmware/terraform-provider-vcd/issues/510#issuecomment-843721455
+    initscript                 = <<-EOT
+    ssh_pwauth: true
+    packages:
+    - open-iscsi
+    runcmd:
+    - systemctl enable iscsid.service
+    - systemctl start iscsid.service
+    EOT
     # TODO: open-iscsi/iscsiadm installation! https://longhorn.io/docs/1.3.0/deploy/install/#using-the-environment-check-script
     # TODO: https://longhorn.io/docs/1.3.0/deploy/install/
   }
@@ -90,6 +110,13 @@ resource "vcd_vapp_vm" "k8s_worker" {
   accept_all_eulas = true
   power_on         = true
 
+  override_template_disk {
+    bus_type        = "paravirtual"
+    size_in_mb      = "245760"
+    bus_number      = 0
+    unit_number     = 0
+  }
+  
   network {
     type               = "org"
     name               = vcd_network_routed_v2.k8s_nodes.name
@@ -102,6 +129,15 @@ resource "vcd_vapp_vm" "k8s_worker" {
     allow_local_admin_password = true
     auto_generate_password     = false
     admin_password             = var.k8s_worker_root_password
+    # TODO: or maybe this needs to be done like this: https://github.com/vmware/terraform-provider-vcd/issues/510#issuecomment-843721455
+    initscript                 = <<-EOT
+    ssh_pwauth: true
+    packages:
+    - open-iscsi
+    runcmd:
+    - systemctl enable iscsid.service
+    - systemctl start iscsid.service
+    EOT
     # TODO: open-iscsi/iscsiadm installation! https://longhorn.io/docs/1.3.0/deploy/install/#using-the-environment-check-script
     # TODO: https://longhorn.io/docs/1.3.0/deploy/install/
   }
