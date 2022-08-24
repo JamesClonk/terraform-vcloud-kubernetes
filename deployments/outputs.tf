6478ccebc3675fb368bdbbbf34e8b679bbb0ae00
JamesClonk <jamesclonk@jamesclonk.ch>
2022-08-24 15:25:12 +0200

    format grafana output

--- deployments/outputs.tf
+++ deployments/outputs.tf
@@ -6,7 +6,7 @@ output "kubernetes_dashboard_url" {
 }
 
 output "grafana_admin_password" {
-  value = "kubectl -n grafana get secret grafana -o jsonpath='{.data.admin-password}' | base64 -d"
+  value = "kubectl -n grafana get secret grafana -o jsonpath='{.data.admin-password}' | base64 -d; echo"
 }
 output "grafana_url" {
   value = "https://grafana.${var.domain_name != "" ? var.domain_name : "${var.loadbalancer_ip}.nip.io"}"
