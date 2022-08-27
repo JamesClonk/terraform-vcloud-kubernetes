99b2b2aace01cc004395eb0b73ec850bbe1b8ac4
JamesClonk <jamesclonk@jamesclonk.ch>
2022-08-27 16:20:23 +0200

    don't try to load a kubeconfig

--- deployments/main.tf
+++ deployments/main.tf
@@ -37,6 +37,7 @@ provider "kubectl" {
   cluster_ca_certificate = var.cluster_ca_certificate
   client_certificate     = var.client_certificate
   client_key             = var.client_key
+  load_config_file       = false
   apply_retry_count      = 5
 }
 
