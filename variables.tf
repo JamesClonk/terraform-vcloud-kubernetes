00cf05f29ad4a3b04045c1ac060e24d5a7997e4a
Marius Munteanu <marius.munteanu@swisscom.com>
2022-12-02 08:45:16 +0100

    enforce daily log rotation on all infrastructure, seven days retention

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@
 # ======================================================================================================================
 variable "module_version" {
   description = "Version/Release of this module"
-  default     = "v2.4.1"
+  default     = "v2.4.2"
   # See https://github.com/swisscom/terraform-dcs-kubernetes/releases
 }
 
